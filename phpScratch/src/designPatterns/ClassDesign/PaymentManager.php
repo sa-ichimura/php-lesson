e98789cdb6f364fce6f8a284b4185a93fe2e634c
sa-ichimura <qwe.098.zxc@icloud.com>
2022-11-27 20:48:10 +0900

    mehtod with the same problem as the static method

---
+++ phpScratch/src/designPatterns/ClassDesign/PaymentManager.php
@@ -0,0 +1,16 @@
+<?php
+class PaymentManager{
+    private int $discounRate; //割引率
+
+    /**
+     * staticがついていないが、static methodと同じ問題があるメソッド
+     * staticを付与しても問題なく動く場合、実質的にstatic method
+     * @param integer $monyAmount1
+     * @param integer $monyAmount2
+     * @return integer
+     */
+    public function add(int $monyAmount1, int $monyAmount2): int
+    {
+        return $monyAmount1 + $monyAmount2;
+    }
+}
